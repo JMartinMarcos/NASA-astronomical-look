f1a6bc04e5d25de52c8058794b52cd27a233353c
Jorge Martin <jorge.martin@eydevelopment.com>
2019-10-16 15:59:49 +0200

    Create first View

---
+++ app/src/main/java/com/jmm/nasaastronomicallook/AstronomicalPictureDetail.kt
@@ -0,0 +1,12 @@
+package com.jmm.nasaastronomicallook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AstronomicalPictureDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_astronomical_picture_detail)
+    }
+}
