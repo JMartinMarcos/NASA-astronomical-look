6d08e4012fc7d21b9ca5151639883ed8a44a972f
sathood-yom <jmartin.marcos@gmail.com>
2022-06-01 18:01:34 +0200

    Update gradle an dependencies version

--- app/src/main/java/com/jmm/nasaastronomicallook/pictureoftheday/AstronomicalPictureDetailActivity.kt
+++ app/src/main/java/com/jmm/nasaastronomicallook/pictureoftheday/AstronomicalPictureDetailActivity.kt
@@ -8,24 +8,28 @@ import androidx.lifecycle.Observer
 import com.jmm.nasaastronomicallook.R
 import com.jmm.nasaastronomicallook.databinding.ActivityAstronomicalPictureDetailBinding
 import com.jmm.nasaastronomicallook.pictureoftheday.viewModel.AstronomicalPictureOfTheDayViewModel
-import org.koin.android.scope.currentScope
-import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.android.scope.AndroidScopeComponent
+import org.koin.androidx.scope.activityScope
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.scope.Scope
 
-class AstronomicalPictureDetailActivity : AppCompatActivity() {
+class AstronomicalPictureDetailActivity : AppCompatActivity(), AndroidScopeComponent {
 
+    override val scope : Scope by activityScope()
     private lateinit var binding: ActivityAstronomicalPictureDetailBinding
-    private val viewModel: AstronomicalPictureOfTheDayViewModel by currentScope.viewModel(this)
+    private val viewModel: AstronomicalPictureOfTheDayViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_astronomical_picture_detail)
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_astronomical_picture_detail)
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
 
         viewModel.errorResponse.observe(this, Observer { error ->
-           error?.let {
-               Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-           }
+            error?.let {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+            }
         })
     }
 }
